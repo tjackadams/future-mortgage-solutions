f9be0bbc283e78bb16925d6e9c4507745a7b2c2a
Thomas Adams <tjackadams@itadams.co.uk>
2026-06-24 19:13:47 +0100

    Allow production hosts for Angular SSR

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,6 @@
 import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
 
-const angularApp = new AngularAppEngine({
-	// It is safe to set allow `localhost`, so that SSR can run in local development,
-	// as, in production, Cloudflare will ensure that `localhost` is not the host.
-	allowedHosts: ['localhost'],
-});
+const angularApp = new AngularAppEngine();
 
 /**
  * This is a request handler used by the Angular CLI (dev-server and during build).
